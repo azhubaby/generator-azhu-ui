d08fd993b81460500d3dbc8f2275fa5712b31f66
johnny <t511069160@qq.com>
2019-11-15 16:04:38 +0800

    'first_commit'

---
+++ src/Button/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+export default class extends React.Component {
+    static propTypes = {
+        type: PropTypes.oneOf(["primary", "default", "danger"]),
+        disabled: PropTypes.oneOf([true, false]),
+        size: PropTypes.oneOf(["md", "lg", "sm"])
+    };
+
+    static defaultProps = {
+        type: "default",
+        disabled: false,
+        size: "md"
+    };
+
+    render() {
+        const { type, disabled, size, children, onClick } = this.props;
+        let _classType = `azhu-btn-${type}`
+        let _classSize = `azhu-btn-${size}`
+
+        let _class = `azhu-btn ${_classType} ${_classSize}`;
+
+        return (
+            <button onClick={onClick} disabled={disabled} className={_class}>
+                {children}
+            </button>
+        );
+    }
+}
